2fe9c33ed69a00936299694731beea08c930401f
Siju <dlsiju@gmail.com>
2025-02-21 18:04:35 +0530

    Added multiplication method

--- src/main/java/com/example/demo/service/HomeService.java
+++ src/main/java/com/example/demo/service/HomeService.java
@@ -9,4 +9,9 @@ public class HomeService {
   public String add(String num1, String num2) {
     return String.valueOf(Integer.parseInt(num1) + Integer.parseInt(num2));
   }
+
+  /**/
+  public String mull(String first, String second) {
+    return String.valueOf(Integer.parseInt(first) + Integer.parseInt(second));
+  }
 }
